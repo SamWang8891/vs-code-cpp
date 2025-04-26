ae7838f8432fbf6642c3c3a60aca173e4746107f
SamWang8891 <g348.8891@smashit.tw>
2025-04-26 22:01:56 +0800

    feat: initial commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "hello World";
+
+    int a = 0;
+
+    if(a == 0){
+        cout << "Testtt";
+    }
+    else {
+        cout << "ttt";
+    }
+    return 0;
+}
